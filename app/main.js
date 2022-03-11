1188822e4378aeff4dd21d62ac492ca71876be82
hugo <hilairehugo@gmail.com>
2022-03-11 14:19:23 +0100

    Add multi input support

--- app/main.js
+++ app/main.js
@@ -27,8 +27,10 @@ try {
   pane.importPreset(JSON.parse(preset));
 } catch (ignore) {}
 
-let lidar = [];
-socket.on('lidar-data', (d) => { lidar = d; });
+let lidar = {};
+socket.on('lidar-data', ({ id, data }) => {
+  lidar[id] = data;
+});
 
 function d2r (deg) {
   return deg * (Math.PI / 180);
@@ -39,26 +41,28 @@ const render = function () {
   context.fillStyle = 'white';
   context.fillRect(0, 0, canvas.width, canvas.height);
 
-  context.save();
-  context.translate(
-    (canvas.width / 2) + params.offsetX,
-    (canvas.height / 2) + params.offsetY
-  );
-  context.rotate(d2r(params.rotation));
+  for (const id in lidar) {
+    context.save();
+    context.translate(
+      (canvas.width / 2) + params.offsetX,
+      (canvas.height / 2) + params.offsetY
+    );
+    context.rotate(d2r(params.rotation));
 
-  context.fillStyle = 'red';
-  context.fillRect(0, 0, 10, 10);
-  context.fillStyle = 'black';
+    context.fillStyle = 'red';
+    context.fillRect(0, 0, 10, 10);
+    context.fillStyle = 'black';
 
-  for (let i = (lidar.length - 1); i > 0 ; i--) {
-    let { x, y } = lidar[i];
+    for (let i = (lidar[id].length - 1); i > 0 ; i--) {
+      let { x, y } = lidar[id][i];
 
-    x /= params.scale;
-    y /= params.scale;
+      x /= params.scale;
+      y /= params.scale;
 
-    lidar[i] && context.fillRect(x, y, 3, 3);
+      context.fillRect(x, y, 3, 3);
+    }
+    context.restore();
   }
-  context.restore();
 
   requestAnimationFrame(render);
 }
@@ -69,13 +73,6 @@ document.body.appendChild(canvas);
 document.addEventListener('keypress', (e) => {
   if (e.key === 'g') {
     pane.hidden = !pane.hidden;
-  } else if (e.key === 'r') {
-    render();
-  } else if (e.key === 'd') {
-    for (let i = (lidar.length - 1); i > 0 ; i--) {
-      const { a, d } = lidar[i];
-      console.log(i, a.toFixed(2), d);
-    }
   } else if (e.key === 'l') {
     console.log(lidar);
   }
--- index.mjs
+++ index.mjs
@@ -9,10 +9,11 @@ const io = new Server(httpServer, { /* options */ });
 app.use(express.static('app'));
 
 io.on("connection", (socket) => {
-  console.log('socket connected.', socket.id);
+  const { id } = socket;
+  console.log('socket connected.', id);
 
   socket.on('data', (data) => {
-    socket.broadcast.emit('lidar-data', data);
+    socket.broadcast.emit('lidar-data', { id, data });
   });
 });
 httpServer.listen(3000);
