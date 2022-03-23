2f7e59a164de6e1a3e57c1009ed19de873a671e1
hugo <hilairehugo@gmail.com>
2022-03-23 16:32:39 +0100

    wip - add gammacv pipeline

--- client/main.js
+++ client/main.js
@@ -2,13 +2,34 @@ import { io } from 'socket.io-client';
 const socket = io('http://127.0.0.1:3000');
 
 import { Pane } from 'tweakpane';
+import * as EssentialsPlugin from '@tweakpane/plugin-essentials';
+
+import * as gm from 'gammacv';
 
 const canvas = document.createElement('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const context = canvas.getContext('2d');
 
+const gmSession = new gm.Session();
+const input = new gm.Tensor('uint8', [canvas.height, canvas.width, 4]);
+const prevInput = new gm.Tensor('uint8', [canvas.height, canvas.width, 4]);
+let pipeline = input
+pipeline = gm.grayscale(pipeline);
+// pipeline = gm.erode(pipeline, [8, 8]);
+// pipeline = gm.dilate(pipeline, [8, 8]);
+// pipeline = gm.adaptiveThreshold(pipeline);
+// pipeline = gm.motionDetect(input, prevInput);
+gmSession.init(pipeline);
+const gmOutput = gm.tensorFrom(pipeline);
+
 const pane = new Pane({ title: 'opts' });
+pane.registerPlugin(EssentialsPlugin);
+const fpsGraph = pane.addBlade({
+  view: 'fpsgraph',
+  label: 'fpsgraph',
+  lineCount: 2,
+});
 
 let preset = localStorage.getItem('preset');
 try {
@@ -82,7 +103,10 @@ function d2r (deg) {
   return deg * (Math.PI / 180);
 }
 
+let time = 0;
 const render = function () {
+  fpsGraph.begin();
+
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.fillStyle = 'rgba(255, 255, 255, 0.5)';
   context.fillRect(0, 0, canvas.width, canvas.height);
@@ -112,6 +136,20 @@ const render = function () {
     context.restore();
   }
 
+  // const imgData = context.getImageData(0, 0, input.shape[1], input.shape[0]);
+  // input.assign(new Uint8Array(imgData.data));
+
+  // context.clearRect(0, 0, canvas.width, canvas.height);
+  // context.fillStyle = 'rgba(255, 255, 255, 0.5)';
+  // context.fillRect(0, 0, canvas.width, canvas.height);
+
+  // gmSession.runOp(pipeline, time, gmOutput);
+  // gm.canvasFromTensor(canvas, gmOutput);
+
+  // prevInput.assign(new Uint8Array(imgData.data));
+
+  time++;
+  fpsGraph.end();
   requestAnimationFrame(render);
 }
 render();
--- server/index.mjs
+++ server/index.mjs
@@ -7,21 +7,22 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: [
-      "http://127.0.0.1:1234",
-      "http://localhost:1234",
+      'http://127.0.0.1:1234',
+      'http://localhost:1234',
     ]
   }
 });
 
 app.use(express.static('app'));
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   const { id } = socket;
   console.log('socket connected.', id);
 
   socket.broadcast.emit('register', id);
 
-  socket.on("disconnect", () => {
+  socket.on('disconnect', () => {
+    console.log('socket disconnected.', id);
     socket.broadcast.emit('unregister', id);
   });
 
